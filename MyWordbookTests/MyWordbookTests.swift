c2f5c97d047115fe2dae57d24501e5c14392bc3e
220v-K <jwlee010222@gmail.com>
2024-10-28 14:22:58 +0900

    Initial Commit

---
+++ MyWordbookTests/MyWordbookTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyWordbookTests.swift
+//  MyWordbookTests
+//
+//  Created by 이재원 on 10/28/24.
+//
+
+import Testing
+
+struct MyWordbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
